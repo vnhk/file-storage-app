2ad1bc93e60ace60157878778d1d18e6d3829a14
berluk12@gmail.com <berluk12@gmail.com>
2025-10-16 23:58:13 +0200

    CORE-82407 File Storage app yml config

--- src/main/java/com/bervan/filestorage/model/FileDeleteException.java
+++
@@ -1,7 +0,0 @@
-package com.bervan.filestorage.model;
-
-public class FileDeleteException extends RuntimeException {
-    public FileDeleteException(String message) {
-        super(message);
-    }
-}
